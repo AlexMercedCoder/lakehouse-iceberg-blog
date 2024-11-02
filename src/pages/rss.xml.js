3b61aaa52cab4a04c83944e8504bf5eb538f6e6e
Alex Merced <alex@alexmerced.dev>
2024-11-02 09:47:04 -0400

    update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,6 +3,11 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   const blog = await getCollection('blog');
+
+  function generateSummary(content, length = 150) {
+    return content.length > length ? content.slice(0, length) + '...' : content;
+  }
+
   return rss({
     title: 'Alex\'s Apache Iceberg Blog',
     description: 'Alex Merced writes about the Data Lakehouse and Apache Iceberg',
@@ -10,12 +15,11 @@ export async function GET(context) {
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDatetime,
-      description: post.data.description,
+      description: generateSummary(post.body, 150),
       author: post.data.author,
       category: post.data.category,
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
       link: `/blog/${post.slug}/`,
+      content: post.body,
     })),
   });
 }
